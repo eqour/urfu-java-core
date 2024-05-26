179e33354c800acfe358f771eb2f6409ddb8debc
Egor <eqour@yandex.ru>
2024-05-26 21:31:51 +0500

    complete leetcode task 39

---
+++ src/ru/eqour/leetcode/Task39.java
@@ -0,0 +1,29 @@
+package ru.eqour.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Task39 {
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        rec(candidates, target,0, ans, list);
+        return ans;
+    }
+
+    private void rec(int[] arr, int target, int idx, List<List<Integer>> ans, List<Integer> list){
+        if (idx == arr.length){
+            if (target == 0){
+                ans.add(new ArrayList<>(list));
+            }
+            return;
+        }
+        if (arr[idx] <= target){
+            list.add(arr[idx]);
+            rec(arr, target - arr[idx], idx, ans, list);
+            list.remove(list.size() - 1);
+        }
+        rec(arr, target, idx + 1, ans, list);
+    }
+}
